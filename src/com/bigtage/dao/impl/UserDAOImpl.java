94e0b4eaa5a8feb0aaac0e239cc804224298d582
bigtage <896932646@qq.com>
2015-10-27 23:01:31 +0800

    2015年10月27日23:01:20

--- src/com/bigtage/dao/impl/UserDAOImpl.java
+++ src/com/bigtage/dao/impl/UserDAOImpl.java
@@ -2,46 +2,95 @@ package com.bigtage.dao.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import com.bigtage.bean.User;
 import com.bigtage.dao.UserDAO;
-import com.bigtage.util.SystemHibernateSupport;
 
 @Repository
-@SuppressWarnings("unchecked")
-public class UserDAOImpl extends SystemHibernateSupport implements UserDAO {
+public class UserDAOImpl implements UserDAO {
+
+	@Resource
+	private SessionFactory sessionFactory;
 
 	@Override
 	public boolean save(User user) {
-		return (boolean) getHibernateTemplate().save(user);
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public List<User> findById(int uid) {
-		return (List<User>) getHibernateTemplate().find(
-				"from User where uid='" + uid + "'");
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<User> findByNameAndPwd(String name, String pwd) {
-		return (List<User>) getHibernateTemplate().find(
-				"from User where username='" + name + "' and password='" + pwd
-						+ "'");
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tx = null;
+		List<User> users = null;
+		try {
+			tx = session.beginTransaction();
+			users = session.createQuery(
+					"from User u where u.username='" + name
+							+ "' and u.password='" + pwd + "'").list();
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		}
+		return users;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<User> findByEmailAndPwd(String email, String pwd) {
-		return (List<User>) getHibernateTemplate().find(
-				"from User where email='" + email + "' and password='" + pwd
-						+ "'");
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tx = null;
+		List<User> users = null;
+		try {
+			tx = session.beginTransaction();
+			users = session.createQuery(
+					"from User u where u.email='" + email
+							+ "' and u.password='" + pwd + "'").list();
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		}
+		return users;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<User> findByPhoneAndPwd(String phone, String pwd) {
-		return (List<User>) getHibernateTemplate().find(
-				"from User where phone='" + phone + "' and password='" + pwd
-						+ "'");
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tx = null;
+		List<User> users = null;
+		try {
+			tx = session.beginTransaction();
+			users = session.createQuery(
+					"from User u where u.phone='" + phone
+							+ "' and u.password='" + pwd + "'").list();
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		}
+		return users;
 	}
 
 }
